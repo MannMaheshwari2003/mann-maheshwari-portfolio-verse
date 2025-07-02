5ccc6d0faa9b9b534b344f85252de11fdd283060
Mann Maheshwari <mannmaheshwari2003@gmail.com>
2025-07-03 01:45:13 +0530

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -67,9 +67,9 @@ const Footer = () => {
           
           <div className="flex items-center mt-4 md:mt-0">
             <span className="flex items-center">
-              Made with 
+              Made by Mann Maheshwari 
               <Heart className="h-4 w-4 mx-1 text-red-500" /> 
-              and modern web technologies
+              
             </span>
           </div>
         </motion.div>
